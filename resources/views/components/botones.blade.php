c0b7aa76a5a62bb6e0a7d4bc1659d22acdf993d1
Anperezna <tete.perez.moreno@gmail.com>
2025-11-20 17:28:32 +0100

    Registro hecho

--- resources/views/components/botones.blade.php
+++ resources/views/components/botones.blade.php
@@ -14,7 +14,7 @@
 
 @if ($href)
     <a href="{{ $href }}"
-        class="flex items-center gap-2 bg-[{{ $color }}] border-2 border-[{{ $border_color }}] hover:brightness-75 hover:scale-95 text-[black] 
+        class="flex items-center gap-2 bg-[{{ $color }}] border-2 border-[{{ $border_color }}] hover:brightness-75 hover:scale-95 text-[{{ $text_color }}] 
         font-medium rounded-lg px-{{ $size == 'sm' ? '3' : ($size == 'lg' ? '6' : '5') }} py-{{ $height == 'small' ? '2' : ($height == 'large' ? '6' : 
         ($height == 'xlarge' ? '8' : '3')) }} {{ $text_size }} {{ $text_color }} transition-all duration-200 inline-block text-center">
         @if ($img)
@@ -28,5 +28,6 @@
         @if ($img)
             <img src="{{ asset($img) }}" alt="icon" class="w-5 h-5">
         @endif
+        {{ $text }}
     </button>
 @endif
---
+++ resources/views/components/input.blade.php
@@ -0,0 +1,7 @@
+@props([
+    'width'=>'w-full',
+])
+
+<input type="text" class="border-2 border-black rounded-lg px-4 py-2 {{ $width }} focus:outline-none focus:ring-2 focus:ring-blue-500">
+    
+</input>
