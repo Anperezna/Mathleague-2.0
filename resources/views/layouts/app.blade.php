1ec64443229493f7ebffb4a75c854e7c7ff8b5b2
Enzologico2425 <earayac2425@politecnics.barcelona>
2025-11-20 17:39:13 +0100

    Cambios

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,19 +5,31 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    <title>{{ $title ?? 'Math League' }}</title>
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Lilita+One&display=swap" rel="stylesheet">
 
     <script src="https://cdn.tailwindcss.com"></script>
     <script>
         tailwind.config = {
             theme: {
-                extend: {},
+                extend: {
+                    fontFamily: {
+                        'lilita': ['"Lilita One"', 'cursive'],
+                    },
+                },
             },
         };
     </script>
+
+    @stack('styles')
 </head>
 
-<body class="h-screen overflow-hidden">
+<body class="bg-[#111111] min-h-screen overflow-x-hidden">
     @yield('content')
+    @stack('scripts')
 </body>
 
 </html>
