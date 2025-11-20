ca34932e6e6f6463d859897d3f9f103f51dbe7d8
Anperezna <tete.perez.moreno@gmail.com>
2025-11-20 16:19:25 +0100

    Archivos creados

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,37 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
+})->name('index');
+
+Route::get('/', function () {
+    return view('login');
+});
+
+Route::get('/', function () {
+    return view('register');
+});
+
+Route::get('/', function () {
+    return view('mathbus');
+});
+
+Route::get('/', function () {
+    return view('mathmatch');
+});
+
+Route::get('/', function () {
+    return view('cortacesped');
+});
+
+Route::get('/', function () {
+    return view('entrevista');
+});
+
+Route::get('/', function () {
+    return view('perfil');
+});
+
+Route::get('/', function () {
+    return view('juegos');
 });
