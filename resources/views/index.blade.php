852ad27b1bba89d66256c543e600d0243530fcf7
Anperezna <tete.perez.moreno@gmail.com>
2025-11-20 16:51:20 +0100

    navbar creado, falta mejorar

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,6 +1,9 @@
 @extends('layouts.app')
 
 @section('content')
-    <x-botones></x-botones>
-
+    <div class="h-full w-full bg-[url('/image.jpg')] bg-cover bg-center flex flex-col">
+        <x-navbar bg_color="#111111">
+        <x-botones></x-botones>
+        </x-navbar>
+    </div>
 @endsection
