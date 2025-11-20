2fb321f59d6acc249296c22da960ebd3896c6cba
danibarrf <daniel.barrufet@gmail.com>
2025-11-20 16:21:46 +0100

    modelos creados

---
+++ app/Http/Controllers/JuegosSesionController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Juegos_Sesion;
+use Illuminate\Http\Request;
+
+class JuegosSesionController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Juegos_Sesion $juegos_Sesion)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Juegos_Sesion $juegos_Sesion)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Juegos_Sesion $juegos_Sesion)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Juegos_Sesion $juegos_Sesion)
+    {
+        //
+    }
+}
