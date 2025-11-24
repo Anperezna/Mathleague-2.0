25fc9cbc62874a45a61d2bb7ffcd0eebd9234218
Anperezna <tete.perez.moreno@gmail.com>
2025-11-24 16:10:58 +0100

    Por seguridad

---
+++ resources/views/perfil.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+
+@section('content')
+        <x-navbar/>
+
+    <div class="flex items-center justify-center align-center h-full bg-[url('/img/fondo.png')] bg-cover bg-center">
+    </div>
+
+@endsection
